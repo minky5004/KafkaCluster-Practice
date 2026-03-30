0d2cb7d995b3560f928eed053aae119238e21e33
minky5004 <minky5004@gmail.com>
2026-03-30 14:55:36 +0900

    first commit

---
+++ src/main/java/com/example/kafkaclusterpractice/KafkaClusterPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaclusterpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaClusterPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaClusterPracticeApplication.class, args);
+    }
+
+}
